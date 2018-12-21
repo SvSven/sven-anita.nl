f2bb264279b68914c94f2c004ec7713a345c958d
Sven van Steenis <sven.vsteenis@gmail.com>
2018-12-21 17:04:11 +0100

    update post-wedding
    Apparently the countdown library does count...up(?) as well? May as well
    have that as a placeholder.

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,70 +1,9 @@
-// Enable functionality of mobile navbar burger
-var mobileMenu = document.getElementById('mobile-menu');
-var mobileMenuIcon = document.querySelector('.navbar-burger');
-mobileMenuIcon.addEventListener('click', function() {
-    this.classList.toggle('is-active');
-    mobileMenu.classList.toggle('is-active');
-});
+const formats = ['years', 'months', 'days', 'hours', 'minutes', 'seconds'];
 
-// Single page navigation using animated-scroll-to.js
-// https://github.com/Stanko/animated-scroll-to
-var navbarHeight = document.querySelector('.navbar').offsetHeight;
-var navLinks = document.querySelectorAll('.navbar-item');
-navLinks.forEach(function(el) {
-    el.addEventListener('click', function() {
-        var target = document.querySelector(el.dataset.anchor);
-        var options = {
-            offset: parseInt('-' + navbarHeight)
+const timerId = countdown(new Date('2018-10-08 15:30'), function(ts) {
+    for (let [key, value] of Object.entries(ts)) {
+        if(formats.includes(key)) {
+            document.getElementById(key).innerHTML = value + ' ' + key;
         }
-        animateScrollTo(target, options);
-
-        if(mobileMenu.classList.contains('is-active')) {
-            mobileMenu.classList.toggle('is-active');
-            mobileMenuIcon.classList.toggle('is-active');
-        }
-    });
-});
-
-var options = {
-    strings: ["Ja, vi skal gifte oss.", "Yes, we are getting married.", "Ja, wij gaan trouwen."],
-    typeSpeed: 40,
-    backSpeed: 20,
-    backDelay: 2500,
-    loop: true,
-    showCursor: true
-}
-
-var typed = new Typed(".typed-title", options);
-
-var timerId = countdown(new Date('2018-10-08'), function(ts) {
-    document.querySelector('.days').innerHTML = ts.days;
-    document.querySelector('.hours').innerHTML = ts.hours;
-    document.querySelector('.minutes').innerHTML = ts.minutes;
-    document.querySelector('.seconds').innerHTML = ts.seconds;
-}, countdown.MONTHS|countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
-
-var footerBtn = document.querySelector('.dismiss-footer');
-footerBtn.addEventListener('click', function(){
-    document.querySelector('.sticky-footer').style.display = "none";
-});
-
-function initMap() {
-	map = new L.Map('map');
-	map.setView(new L.LatLng(52.088778, 5.092849), 9);
-
-	var tileUrl = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png';
-	var tileAttrb = '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>';
-	var mapTiles = new L.TileLayer(tileUrl, { minZoom: 8, attribution: tileAttrb });
-    map.addLayer(mapTiles);
-
-    L.marker([51.816432, 5.021330]).bindTooltip("Slot Loevestein", {
-        permanent: true,
-        direction: 'right',
-    }).addTo(map);
-
-    L.marker([52.309494, 4.763654]).bindTooltip("Amsterdam Schiphol Airport", {
-        permanent: true,
-        direction: 'right',
-    }).addTo(map);
-}
-initMap();
+    }
+}, countdown.YEARS|countdown.MONTHS|countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
